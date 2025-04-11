81175f738b4888b6ba23c856450bc37d4d079dc8
KOTAPHANITEJA <phanitejakota@gmail.com>
2025-04-11 13:37:03 +0530

    Fifth commit

--- ai_recommender/settings.py
+++ ai_recommender/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-22bjg)p6gqe7(0$1euaujevh65xcfna6l($9umvb-p7lycu3-p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AIBLP.onrender.com','127.0.0.1']
 
 
 # Application definition
